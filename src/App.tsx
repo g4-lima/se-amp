71c4f4e095fa6b07f38de2f7c156951cb1ec386b
Gabriel Lima <gabastos.lima@gmail.com>
2021-04-29 23:43:41 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function App() {
-    return <h1>Hello</h1>;
-}
+import GlobalStyle from './assets/styles/global';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+            <GlobalStyle />
+        </>
+    );
+};
 
 export default App;
