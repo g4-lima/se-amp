2e61f23ea64fe03dfa41d1c614a5350c5b9905cc
Gabriel Lima <gabastos.lima@gmail.com>
2021-05-11 17:16:16 -0300

    about

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-import { Container } from './styles';
+import { Container, TopBar } from './styles';
 
 const About: React.FC = () => {
+    const history = useHistory();
+    const goBack = () => {
+        history.goBack();
+    };
+
     return (
         <>
             <Header />
-            <Container />
+            <Container>
+                <TopBar>
+                    <button type="button" onClick={goBack}>
+                        <IoIosArrowBack size={24} color="#F22020" />
+                        <p>Voltar</p>
+                    </button>
+                </TopBar>
+            </Container>
             <Footer />
         </>
     );
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div`
-    width: 98.9vw;
+    width: 88vw;
+    margin: 20vh auto;
+`;
 
-    margin: auto;
+export const TopBar = styled.div`
+    button {
+        cursor: pointer;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        background: none;
+
+        p {
+            font-family: Montserrat;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 1.2vw;
+
+            color: #808080;
+            margin-left: 1vw;
+
+            &:hover {
+                color: ${shade(0.4, '#808080')};
+            }
+        }
+    }
 `;
