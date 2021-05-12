00dd5387db6d62a8af2d1027460509519628396d
Gabriel Lima <gabastos.lima@gmail.com>
2021-05-12 16:48:51 -0300

    about-page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,7 +5,7 @@ import { IoIosArrowBack } from 'react-icons/io';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-import { Container, TopBar } from './styles';
+import { Container, TopBar, Contents, DescriptionContainer } from './styles';
 
 const About: React.FC = () => {
     const history = useHistory();
@@ -23,6 +23,37 @@ const About: React.FC = () => {
                         <p>Voltar</p>
                     </button>
                 </TopBar>
+                <Contents>
+                    <h1>Trazendo o mundo e o futuro para a América do Sul</h1>
+                    <DescriptionContainer>
+                        <div className="history">
+                            <h3>Nossa história</h3>
+                            <div className="history-box">
+                                <p>
+                                    Localizada em Campinas, São Paulo, a Sharp-e
+                                    atua há mais de 10 anos no mercado. Nós
+                                    atuamos com o fornecimento das principais
+                                    peças de reposição para indústria de papel e
+                                    celulose, com o melhor custo-benefício,
+                                    vindas de fabricantes ao redor do mundo.
+                                </p>
+                                <p>
+                                    Investimos nas melhores parcerias, com
+                                    fornecedores em países como Alemanha,
+                                    Holanda, Dinamarca, Suécia, Brasil, China,
+                                    etc. para garantir qualidade aos nossos
+                                    clientes.
+                                </p>
+                                <p>
+                                    Enquanto a Sharp-e Ltda é a empresa
+                                    responsável pela importação e exportação, a
+                                    AMP, AfterMarketPartner é nossa
+                                    representante comercial.
+                                </p>
+                            </div>
+                        </div>
+                    </DescriptionContainer>
+                </Contents>
             </Container>
             <Footer />
         </>
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -31,3 +31,57 @@ export const TopBar = styled.div`
         }
     }
 `;
+
+export const Contents = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 1.8vw;
+        text-align: center;
+
+        color: #00a62f;
+
+        margin-top: 8vh;
+    }
+`;
+
+export const DescriptionContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    .history {
+        h3 {
+            font-family: Montserrat;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 1.7vw;
+
+            color: #808080;
+        }
+
+        .history-box {
+            background: #f9f9f9;
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            border-radius: 4px;
+
+            width: 50vw;
+            height: 48vh;
+
+            p {
+                font-family: Roboto;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 1.1vw;
+                line-height: 2vw;
+                padding: 3.5vw 6vw 3vw 0vw;
+                text-align: center;
+
+                color: #808080;
+            }
+        }
+    }
+`;
