361958db7dfe902fae6e15b81ad6ce531c753f27
Gabriel Lima <gabastos.lima@gmail.com>
2021-06-07 16:46:26 -0300

    proucts card

--- src/pages/ProductsPM/index.tsx
+++ src/pages/ProductsPM/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
 
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-import { Container, LeftList } from './styles';
+import {
+    Container,
+    LeftList,
+    Content,
+    PageBar,
+    ProductsGroup,
+    ProductsCard,
+} from './styles';
 
 const ProductsPM: React.FC = () => {
     return (
@@ -14,6 +23,34 @@ const ProductsPM: React.FC = () => {
                     <p>Papel e Celulose</p>
                     <p>Máquina de Papel</p>
                 </LeftList>
+                <Content>
+                    <PageBar>
+                        <p>Papel e Celulose</p>
+                        <IoIosArrowForward size={20} color="#F22020" />
+                        <p>Máquina de Papel</p>
+                    </PageBar>
+
+                    <ProductsGroup>
+                        <h2>Preparo de Massa</h2>
+                        <ProductsCard>
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                        </ProductsCard>
+                    </ProductsGroup>
+
+                    <ProductsGroup>
+                        <h2>Seção de Formação</h2>
+                        <ProductsCard>
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                            <Link to="/productsPM" className="card" />
+                        </ProductsCard>
+                    </ProductsGroup>
+                </Content>
             </Container>
             <Footer />
         </>
--- src/pages/ProductsPM/styles.ts
+++ src/pages/ProductsPM/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 88vw;
     margin: 23vh auto;
+    display: flex;
+    flex-direction: row;
 `;
 
 export const LeftList = styled.div`
@@ -10,6 +12,73 @@ export const LeftList = styled.div`
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 4px;
 
-    width: 16vw;
+    width: 18.5vw;
     height: 120vh;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    margin-left: 2.8vw;
+`;
+
+export const PageBar = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    top: 0;
+    height: 4vh;
+    width: 100%;
+
+    p {
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 1.2vw;
+
+        color: #808080;
+        margin-right: 1vw;
+    }
+
+    svg {
+        margin-right: 1vw;
+    }
+`;
+
+export const ProductsGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    margin-left: 0.5vw;
+    margin-top: 8vh;
+
+    h2 {
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 2vw;
+
+        color: #00a62f;
+    }
+`;
+
+export const ProductsCard = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-gap: 1.6vw;
+
+    margin-top: 3vh;
+
+    .card {
+        width: 12vw;
+        height: 25vh;
+
+        background: #f9f9f9;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 4px;
+    }
 `;
