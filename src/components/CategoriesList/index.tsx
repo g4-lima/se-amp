8fbe68903040f18f1d62166838cc344cee8b35ae
Gabriel Lima <gabastos.lima@gmail.com>
2021-06-18 15:43:04 -0300

    side bar hide/show

--- src/components/CategoriesList/index.tsx
+++ src/components/CategoriesList/index.tsx
@@ -4,7 +4,7 @@ import { Container } from './styles';
 import { List } from './data';
 
 const CategoriesList: React.FC = () => {
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(0);
 
     return (
         <Container>
@@ -12,9 +12,12 @@ const CategoriesList: React.FC = () => {
                 <h1>
                     {item.category}
                     {item.sections.map(section => (
-                        <button type="button" onClick={() => setShow(!show)}>
+                        <button
+                            type="button"
+                            onClick={() => setShow(section.id)}
+                        >
                             {section.name}
-                            {show
+                            {show === section.id
                                 ? section.products.map(product => (
                                       <p>{product.name}</p>
                                   ))
