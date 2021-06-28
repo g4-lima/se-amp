31c9a15c6911e821516b656c372295bc2a756451
Gabriel Lima <gabastos.lima@gmail.com>
2021-06-28 15:49:48 -0300

    width

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,7 +7,7 @@ export const BackgroundImage = styled.div`
     position: relative;
 
     img {
-        width: 98.9vw;
+        width: 98.8vw;
         height: 100vh;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         transform: matrix(-1, 0, 0, 1, 0, 0);
