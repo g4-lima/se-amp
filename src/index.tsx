f903c384ffb682ac82af3f77b1e9321eae0ed8b5
Gabriel Lima <gabastos.lima@gmail.com>
2021-08-13 16:07:48 -0300

    ga4

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GA4React from 'ga-4-react';
 import App from './App';
+
+const ga4react = new GA4React('G-FJ9P97S85W');
+
+ga4react.initialize();
 
 ReactDOM.render(
     <React.StrictMode>
